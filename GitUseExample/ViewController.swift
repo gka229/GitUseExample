61805191ae77d840705b331e7f4dacf1d235529c
Guerschom Kayembe <mckayguerschom22@gmail.com>
2020-08-27 00:08:45 -0400

    Initial Commit

---
+++ GitUseExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitUseExample
+//
+//  Created by guerschom m kayembe on 8/27/20.
+//  Copyright © 2020 CS485. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
