28310c02dfdad2af9d71b91706e9a99d585b8aea
Guerschom Kayembe <mckayguerschom22@gmail.com>
2020-08-27 00:45:50 -0400

    Add new map-for-Itinerary feature

---
+++ GitUseExample/MapForltinerary.swift
@@ -0,0 +1,13 @@
+//
+//  MapForltinerary.swift
+//  GitUseExample
+//
+//  Created by guerschom m kayembe on 8/27/20.
+//  Copyright © 2020 CS485. All rights reserved.
+//
+
+//import Foundation
+import UIKit
+class MapForItinerary: NSObject{
+    
+}
